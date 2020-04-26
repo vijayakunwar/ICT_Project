c1741bc4eae1ec654093c8739ff599eb4588f4b7
vijayakunwar <kunwar.vijaya@gmail.com>
2020-04-27 00:11:21 +1000

    test for heroku push

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ICT_Project</title>
+</head>
+<body>
+	<h1>ICT Project : SP21 2020 </h1>
+	<h2>Vijaya Kunwar</h2>	
+
+	<?php
+	echo phpinfo();
+	?>
+</body>
+</html>
