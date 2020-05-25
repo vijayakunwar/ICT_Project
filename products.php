e96e40773dbb8de42720c44b1eb7c3fcc66c6fb6
vijayakunwar <kunwar.vijaya@gmail.com>
2020-05-25 15:54:54 +1000

    product page updated with images

--- products.php
+++ products.php
@@ -1,5 +1,166 @@
 <?php include('header.php'); ?>
 
-<h1>products page- search product and view items</h1>
+<div class="container-fluid">
+  <div class="jumbotron">
+    <h1 class="display-4">We have boxes of small, medium and large size</h1>
+    <p class="lead">Feel free to contact us if need customized boxes</p>
+    <hr class="my-4">
+    <p>We are local farmer freind and promoter.</p>
+    <p class="lead">
+       <a class="btn btn-danger btn-lg" href="#" role="button"> Delivery Area</a>
+       <a class="btn btn-primary btn-lg" href="#" role="button"> Contact US</a>
+
+    </p>
+  </div>
+  
+</div>
+
+
+<dir class="conainter feature-text">
+  <h1>Fruit Boxes</h1>
+  
+</dir>
+
+<div class="container-fluid">
+  <div class="row-fluid row align-items-center justify-content-center">
+
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <div class="card  embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/fruit_box_small.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Fruit Box Small </h5>
+          <h5 class="card-title"> <span>$30.00</span></h5>
+          <p class="card-text">Small Fruit Box fulfilled by local farmer. </p>
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div> 
+
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <div class="card embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/fruit-gift-box.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Fruit Gift Box </span></h5>
+          <h5 class="card-title"> <span>$50.00</span></h5>
+          <p class="card-text">Fruit Gift Box fulfilled by local farmer.</p>
+
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div> 
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <!-- <div class="card embed-responsive embed-responsive-16by9"> -->
+      <div class="card  embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/workplace-fruit-box.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Workplace Fruit Box</h5>
+          <h5 class="card-title"> <span>$70.00</span></h5>
+          <p class="card-text">Workplace Fruit Box fulfilled by local farmer </p>
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<hr>
+
+<dir class="conainter feature-text">
+  <h1>Vegetable Boxes</h1>
+  
+</dir>
+
+<div class="container-fluid">
+  <div class="row-fluid row align-items-center justify-content-center">
+
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <div class="card  embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/veg_35_small.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Vegetable Box Small </h5>
+          <h5 class="card-title"> <span>$35.00</span></h5>
+          <p class="card-text">Small Vegetable Box fulfilled by local farmer. </p>
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div> 
+
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <div class="card embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/veg_50_medium.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Vegetable Box Medium</span></h5>
+          <h5 class="card-title"> <span>$50.00</span></h5>
+          <p class="card-text">Vegetable Box fulfilled by local farmer.</p>
+
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div> 
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <!-- <div class="card embed-responsive embed-responsive-16by9"> -->
+      <div class="card  embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/veg_70_large.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Vegetable Box Large</h5>
+          <h5 class="card-title"> <span>$70.00</span></h5>
+          <p class="card-text">Vegetable Box fulfilled by local farmer </p>
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<hr>
+<dir class="conainter feature-text">
+  <h1>Custmized Fruit and Vegetable Boxes</h1>
+  
+</dir>
+
+<div class="container-fluid">
+  <div class="row-fluid row align-items-center justify-content-center">
+
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <div class="card  embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/salad-fruit-box.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Salad and Fruit Box Small </h5>
+          <h5 class="card-title"> <span>$30.00</span></h5>
+          <p class="card-text"> Choosr items for Salad and Fruit Box. Products are fulfilled by local farmer. </p>
+          <a href="#" class="btn btn-primary">Contact Us</a>
+        </div>
+      </div>
+    </div> 
+
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <div class="card embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/veg_50_custom.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Vegetable Box Medium</span></h5>
+          <h5 class="card-title"> <span>$50.00</span></h5>
+          <p class="card-text">Choose your vegetables. Products are fulfilled by local farmer.</p>
+
+          <a href="#" class="btn btn-primary">Contact Us</a>
+        </div>
+      </div>
+    </div> 
+    <div class="cell col-sm-6 col-md-4 col-lg-3">
+      <!-- <div class="card embed-responsive embed-responsive-16by9"> -->
+      <div class="card  embed-responsive-16by9">
+        <img class="card-img-top embed-responsive-item" src="images/product/fruit_70_custom.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Fruit Box Large</h5>
+          <h5 class="card-title"> <span>$70.00</span></h5>
+          <p class="card-text">Choose your Fruits. Products are fulfilled by local farmer </p>
+          <a href="#" class="btn btn-primary">Contact Us</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
 
 <?php include('footer.php'); ?>
